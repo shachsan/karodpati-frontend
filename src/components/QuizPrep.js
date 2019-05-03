fcd67b97897680ac933e891298476b23dc5cff80
Sanjay Sai <shachsan@hotmail.com>
2019-05-03 13:39:02 -0400

    work with button group

--- src/components/QuizPrep.js
+++ src/components/QuizPrep.js
@@ -5,14 +5,17 @@ import {
     TouchableOpacity, 
     StyleSheet, 
     Picker, 
-    Item 
+    Item,
     } from 'react-native';
+import {ButtonGroup} from 'react-native-elements';
 
 export default class QuizPrep extends Component {
     state={
         form:{
             numOfQuiz:5,
-            categories:[],
+            mainCat:'',
+            selectedCat:[],
+            subCat:[],
             level:''
         }
     }
@@ -20,8 +23,16 @@ export default class QuizPrep extends Component {
     static navigationOptions={
         title:'preparing your quiz'
     }
+
+    selectMainCats=(selectedIndex)=>{
+        console.log('selected Index', selectedIndex);
+        newCat=[...this.state.selectedCat]
+        newCat.push(selectedIndex)
+       this.setState({selectedCat:newCat})
+    }
     render() {
-        // console.log('selected number:', this.state.numOfQuiz);
+        console.log('selected category:', this.state.mainCat);
+        //TODO: dynamically generate below array
         quizQty=[ <Picker.Item key={5} label="5" value="5" />,
                     <Picker.Item key={10} label="10" value="10" />,
                     <Picker.Item key={15} label="15" value="15" />,
@@ -31,18 +42,40 @@ export default class QuizPrep extends Component {
                     <Picker.Item key={35} label="35" value="35" />,
                     <Picker.Item key={40} label="40" value="40" />
                 ]
+        mainCategories=["Internation", "National", "Both"];
+        const {mainCat} = this.state;
+
         return (
              <View style={styles.container}>
                 <Text style={styles.label}>How many questions would you like to attempt?</Text>
                 <Picker
                     selectedValue={this.state.numOfQuiz}
-                    style={{height: 50, width: 100, marginTop:-30}}
+                    style={styles.picker}
                     onValueChange={(itemValue, itemIndex) =>
                         this.setState({numOfQuiz: itemValue})
                     }>{quizQty}
                 </Picker>
 
-                
+                <View style={styles.category}>
+                    <Text>Choose Question Categories</Text>
+                    <ButtonGroup 
+                        onPress={this.selectMainCats}
+                        selectedIndex={mainCat}
+                        buttons={mainCategories}
+                        containerStyle={{height: 100}}
+                        selectMutiple={true}
+                    />
+                    {/* <Button style={styles.mainCatBtns} title="International">International</Button>
+                    <Button style={styles.mainCatBtns} title="National">National</Button>
+                    <Button style={styles.mainCatBtns} title="Both">Both</Button> */}
+                </View>
+                <View style={styles.category}>
+                    <Text>Choose Question Categories</Text>
+                </View>
+                <View style={styles.category}>
+                    <Text>Choose Question Categories</Text>
+                </View>
+
              </View>
         );
     }
@@ -51,7 +84,7 @@ export default class QuizPrep extends Component {
 const styles = StyleSheet.create({
     container:{
         flex:1,
-        // justifyContent:'center',
+        // justifyContent:'flex-start',
         alignItems:'center',
         // flexDirection:'row',
         flexWrap:'wrap'
@@ -64,7 +97,17 @@ const styles = StyleSheet.create({
         // margin:5,
         padding:5
     },
+    picker:{
+        flex:1,
+        height: 50, 
+        width: 100, 
+        marginTop:-30
+    },
 
+    category:{
+        flex:1,
+        justifyContent:'flex-start'
+    },
     header:{
         fontSize:35,
         alignItems:'stretch'
