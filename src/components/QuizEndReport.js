db841c2bba60a52656a613e8bd1bb72e6b3dc694
Sanjay Sai <shachsan@hotmail.com>
2019-05-28 09:53:55 -0400

    dynamically change background color for accuracy chart

--- src/components/QuizEndReport.js
+++ src/components/QuizEndReport.js
@@ -5,20 +5,33 @@ const QuizEndReport = props =>{
     const accuracyData = props.navigation.state.params;//navigation.state.params stores
         //object passed as second argument to navigate methode. Ref: QuizContainer/navigate('QuizEndReport') 
     console.log('End of quiz report:', accuracyData);
+    const accuracyRate = (accuracyData.noOfCorrectAns/accuracyData.totalQuestions)*100;
+    
+    const getColor = ()=>{
+        // console.log('inside getcolor func, accuracyRate', accuracyRate);
+        if(accuracyRate > 70){
+            return 'green';
+        }else{
+            return 'red';
+        }
+    }
+    
     return (
         <View style={styles.container}>
                 <Text>
                     Report
                 </Text>
-            <View style={styles.circle}>
+            <View style={[styles.circle, {backgroundColor:getColor()}]}>
                 <Text style={[styles.accuracy, {fontSize:20}]}>Accuracy</Text>
                 <Text style={styles.accuracy}>
-                    {(accuracyData.noOfCorrectAns/accuracyData.totalQuestions)*100}%
+                    {accuracyRate}%
                 </Text>
             </View>
         </View>
     )
 }
+
+
 
 const styles = StyleSheet.create({
     container:{
@@ -31,7 +44,6 @@ const styles = StyleSheet.create({
         width: 150,
         height: 150,
         borderRadius: 150 / 2,
-        backgroundColor: 'green',
         justifyContent:'center',
     },
     accuracy:{
